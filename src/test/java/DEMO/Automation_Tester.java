08057934807bd8eee1ac8d5d5e2d185558b1e1fe
rajanikumari180681 <rajanikumari180681@gmail.com>
2025-01-27 16:58:05 +0530

    Rajani sent you one class

---
+++ src/test/java/DEMO/Automation_Tester.java
@@ -0,0 +1,9 @@
+package DEMO;
+
+public class Automation_Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This programe is from Rajani");	}
+
+}
